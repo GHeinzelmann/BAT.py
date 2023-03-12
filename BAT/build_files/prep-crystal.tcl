dc537f2da771dfa6fb9c3e8e8baee4d9237c8e14
GHeinzelmann <germanohei@gmail.com>
2023-03-12 10:52:26 -0300

    change prep-crystal file

--- BAT/build_files/prep-crystal.tcl
+++ BAT/build_files/prep-crystal.tcl
@@ -1,7 +1,7 @@
 mol load pdb CCCC.pdb
 set a [atomselect top "chain A and protein and noh"]
 set b [atomselect top "chain A and resname MMM and noh"]
-$a writepdb protein.pdb
+$a writepdb rec_file.pdb
 $b writepdb mmm.pdb
 exit
 
